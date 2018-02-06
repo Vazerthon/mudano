a2ca864249c1e34bdfdd5e9f72ad4545696d36f3
Vallis <paul@mrvallis.co.uk>
2018-02-06 21:22:07 +0000

    Extracts users from entries

--- src/state/reducers/app.js
+++ src/state/reducers/app.js
@@ -1,13 +1,23 @@
+import * as R from 'ramda';
 import { constants } from '../actions/app';
 
 const defaultAppState = {
   entries: [],
+  users: [],
 };
+
+const extractUsersFromEntries = R.pipe(R.uniqBy(x => x.userId), entries =>
+  entries.map(u => ({ userId: u.userId, name: u.name })),
+);
 
 const appReducer = (state = defaultAppState, action) => {
   switch (action.type) {
     case constants.dataLoaded:
-      return { ...state, entries: action.payload.entries };
+      return {
+        ...state,
+        entries: action.payload.entries,
+        users: extractUsersFromEntries(action.payload.entries),
+      };
     default:
       return state;
   }
--- src/state/reducers/app.test.js
+++ src/state/reducers/app.test.js
@@ -19,4 +19,64 @@ describe('app reducer', () => {
 
     expect(result.entries).toEqual(mockData);
   });
+
+  it('extracts users from entry data', () => {
+    const mockData = [
+      {
+        userId: 1,
+        name: 'Matthew Webb',
+        parsedDate: new Date('Thu 2016-12-29'),
+        unit: 'PM',
+        value: 'P',
+      },
+      {
+        userId: 1,
+        name: 'Matthew Webb',
+        parsedDate: new Date('Thu 2016-12-28'),
+        unit: 'PM',
+        value: 'P',
+      },
+      {
+        userId: 2,
+        name: 'Marilyn Bell',
+        parsedDate: new Date('Thu 2016-12-27'),
+        unit: 'PM',
+        value: 'P',
+      },
+      {
+        userId: 3,
+        name: 'Jon Erikson',
+        parsedDate: new Date('Thu 2016-12-26'),
+        unit: 'PM',
+        value: 'P',
+      },
+      {
+        userId: 3,
+        name: 'Jon Erikson',
+        parsedDate: new Date('Thu 2016-12-25'),
+        unit: 'PM',
+        value: 'P',
+      },
+    ];
+
+    const result = reducer({}, actions.dataLoaded(mockData));
+
+    const expected = [
+      {
+        userId: 1,
+        name: 'Matthew Webb',
+      },
+
+      {
+        userId: 2,
+        name: 'Marilyn Bell',
+      },
+      {
+        userId: 3,
+        name: 'Jon Erikson',
+      },
+    ];
+
+    expect(result.users).toEqual(expected);
+  });
 });
